24aba6bf66f4af2a1040f6a7cfad690babee3876
DESKTOP-3598OT8\user <1234>
2024-06-04 14:03:46 +0900

    feature/interface : [modify] interface 로직 상위 내용에 맞게 변경

--- SSD/SSD/SSD_HW.h
+++ SSD/SSD/SSD_HW.h
@@ -3,7 +3,7 @@
 
 interface SSD_HW{
 public:
-	virtual void sector() = 0;
+	virtual void init() = 0;
 	virtual void writeNandFile() = 0;
 	virtual void writeResultFile() = 0;
 	virtual void readNandFile() = 0;
