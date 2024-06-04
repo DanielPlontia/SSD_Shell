649d9e1d7b00b790bad0942f6c79738ccf899553
DESKTOP-5FBE16T\User <intelhs@naver.com>
2024-06-04 14:26:26 +0900

    main merger 용 커밋

--- SSD/SSD/SSD_Invoker.cpp
+++ SSD/SSD/SSD_Invoker.cpp
@@ -1,4 +1,28 @@
-class SSD_Invoker {
-public:
+#include<vector>
+#include<string>
+#include "Command.h"
 
+using std::vector;
+using std::string;
+
+
+
+class SSD_Invoker {
+private:
+	vector<string> userCmd;
+	Command* commandInstance;
+
+
+
+
+public:
+	SSD_Invoker(vector<string> userCommand)
+	{
+		userCmd = userCommand;
+	}
+
+	void run()
+	{
+
+	}
 };
