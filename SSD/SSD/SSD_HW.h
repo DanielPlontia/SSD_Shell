d8975cb39868061d414c3f21019ca4774f589165
Heeju Kim <heeju00627@gmail.com>
2024-06-04 14:25:28 +0900

    feature/ssd: [refactor] redefine SSD_HW interface

--- SSD/SSD/SSD_HW.h
+++ SSD/SSD/SSD_HW.h
@@ -3,8 +3,6 @@
 
 interface SSD_HW{
 public:
-	virtual void sector() = 0;
-	virtual void writeNandFile() = 0;
-	virtual void writeResultFile() = 0;
-	virtual void readNandFile() = 0;
+	virtual void read(int addr) = 0;
+	virtual void write(int addr, unsigned int data) = 0;
 };
