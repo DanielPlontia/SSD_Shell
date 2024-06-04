654cd1a744177f00640be60b3bd66e152eaa71c5
DESKTOP-5FBE16T\User <intelhs@naver.com>
2024-06-04 15:06:28 +0900

    중간 merge

--- SSD/Sample-Test1/SDD_Invoker_test.cpp
+++ SSD/Sample-Test1/SDD_Invoker_test.cpp
@@ -1,2 +1,13 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include "../SSD/SSD_Invoker.cpp"
+#include "../SSD/write.cpp"
+#include<vector>
+
+using namespace std;
+using namespace testing;
+
+public CMD_Mock : public WriteCmd{
+public:
+	MOCK_METHOD(void, execute,(const std::vector<std::string>& operation),());
+}
