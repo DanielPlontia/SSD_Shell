447a08f2f51ffcd9322196207b1bde45e50bb1fd
DESKTOP-5FBE16T\User <intelhs@naver.com>
2024-06-04 15:28:22 +0900

    에러코드 머지

--- SSD/Sample-Test1/SDD_Invoker_test.cpp
+++ SSD/Sample-Test1/SDD_Invoker_test.cpp
@@ -9,22 +9,35 @@
 using namespace std;
 using namespace testing;
 
-class CMD_Write_Mock : public WriteCmd {
+class CMD_Write_Mock : public Command {
 public:
 	MOCK_METHOD(void, execute, (const std::vector<std::string>& operation), ());
 
+private:
+	MOCK_METHOD(bool, check_validation, (), ());
+	MOCK_METHOD(void, do_action, (), ());
 };
 
 
-class CMD_Read_Mock : public ReadCmd {
+class CMD_Read_Mock : public Command {
 public:
 	MOCK_METHOD(void, execute, (const std::vector<std::string>& operation), ());
 
+private:
+	MOCK_METHOD(bool, check_validation, (), ());
+	MOCK_METHOD(void, do_action, (), ());
+};
+
+class writeCmdFixtrue : public testing::Test {
+public:
+	vector<string> userCmd = { "W", "2", "0xAAAABBBB" };
+	CMD_Write_Mock writeCmdMock;
 };
 
 
-TEST(SSD_Invoker_Test, writeInvoke) {
+TEST_F(writeCmdFixtrue, writeInvoke) {
+	CMD_Write_Mock writeCmdMock;
 
-
+	EXPECT_CALL(writeCmdMock, execute).Times(1);
 }
 
