0057805881c108b1683dec3f530362f875350a7a
DanielPlontia <plontia@gmail.com>
2024-06-04 13:48:17 +0900

    [Implement] main

--- SSD/SSD/main.cpp
+++ SSD/SSD/main.cpp
@@ -1,7 +1,16 @@
 #include "SSD_HW.h"
 #include "Command.h"
-int main(int argc, char *argv[]) {
-	// SSD Invoker
-	// Invoker »ý¼º
+#include <vector>
+#include <string>
+#include <stdexcept>
 
+using namespace std;
+int main(int argc, char *argv[]) {
+	vector<string> command;
+	for (int i = 0; i < argc; i++) {
+		command.push_back(argv[i]);
+	}
+	if (argc > 4) throw std::invalid_argument("Too Much Argument");
+	if (command[1] != "R" || command[1] != "W") throw std::invalid_argument("Invalid Operation");
+	Invoker invoker{ command };
 }
