c8c136a234ff7f0dc976cc7d137b3b969462eb95
DESKTOP-3598OT8\user <1234>
2024-06-05 11:03:01 +0900

    feature/testshell_help : [Feature] help command TDD 구현 및 Product 코드 구현

--- SSD/Sample-Test2/test.cpp
+++ SSD/Sample-Test2/test.cpp
@@ -1,29 +1,5 @@
-#include <gtest/gtest.h>
-#include <gmock/gmock.h>
-#include <string>
-#include <stdexcept>
 
-#include "../TestShell/ssdTestShell.cpp"
-
-using namespace std;
-
-class ExeMock : public exeRunner {
-public:
-	MOCK_METHOD(void, runner, (string cmd), (override));
-};
-
-class dataReaderMock : public dataReader {
-public:
-	MOCK_METHOD(string, fileRead, (), ());
-};
-
-class TestShellFixture : public testing::Test {
-public:
-	ExeMock exeMock;
-	dataReaderMock readerMock;
-
-	TestShell shell{ &exeMock, &readerMock };
-};
+#include "TestShellFixture.h"
 
 TEST_F(TestShellFixture, ReadData) {
 	EXPECT_CALL(exeMock, runner).Times(1);
