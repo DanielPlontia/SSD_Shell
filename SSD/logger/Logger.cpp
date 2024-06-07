69ee7756dcc706880cc676e58384478968443b42
DESKTOP-3598OT8\user <1234>
2024-06-07 15:31:55 +0900

    refactoring/logger_filemng : [Refactoring] Log file Manager Class로 File 관련한 기능 분리

--- SSD/logger/Logger.cpp
+++ SSD/logger/Logger.cpp
@@ -7,7 +7,7 @@ void WriteLog(const std::string& funcName, const std::string& msg) {
 
 Logger::Logger()
 {
-    fileMutex = std::make_shared<SharedMutex>("loggerMTX");
+    
 }
 
 void Logger::writelog(const std::string& funcName, const std::string& msg) {
@@ -24,40 +24,8 @@ void Logger::writelog(const std::string& funcName, const std::string& msg) {
 
     std::string formated_str = std::format("{} {:30}: {}\n", buffer, split(funcName, "::").c_str(), msg.c_str());
 
-    std::lock_guard<SharedMutex> lock(*fileMutex.get());
-
-    if (!std::filesystem::exists(log_file)) {
-        std::filesystem::create_directory(log_file.parent_path());
-    }
-    else if (std::filesystem::file_size(log_file) > 10 * 1024) //10kb ³ÑÀ»‹š
-    {
-        char newFileName[100];
-        std::strftime(newFileName, sizeof(newFileName), "until_%y%m%d_%Hh_%Mm_%Ss.log", &localTime);
-        std::filesystem::path newfile_fullname = log_file.parent_path() / std::string(newFileName);
-
-        existFileList.clear();
-        findLogFiles(log_file.parent_path());
-        if (existFileList.size() >= 2)
-        {
-            for (auto now : existFileList )
-            {
-                if (now == log_file)
-                    continue;
-
-                std::filesystem::path oldPath = now;
-                std::filesystem::rename(oldPath, now.replace_extension(".zip"));
-            }
-        }
-
-        std::filesystem::rename(log_file, newfile_fullname);
-    }
-
-    std::ofstream file(log_file, std::ios::app);
-    if (file.is_open()) {
-        std::cout << msg << std::endl;
-        file.write(formated_str.c_str(), formated_str.length());
-        file.close();
-    }
+    file_mng.WriteFile(formated_str);
+    std::cout << msg << std::endl;
 }
 
 std::string Logger::split(std::string_view str, std::string_view delim) {
@@ -69,14 +37,4 @@ std::string Logger::split(std::string_view str, std::string_view delim) {
 
     std::vector<std::string> strings{ view.begin(), view.end() };
     return strings[1] + "()";
-}
-
-void Logger::findLogFiles(const std::filesystem::path& directory) {
-    for (const auto& entry : std::filesystem::directory_iterator(directory)) {
-        if (!entry.is_directory()) {
-            if (entry.path().has_extension() && entry.path().extension() == ".log") {
-                existFileList.push_back(entry.path());
-            }
-        }
-    }
 }
--- SSD/logger/Logger.h
+++ SSD/logger/Logger.h
@@ -7,10 +7,8 @@
 #include <format>
 #include <vector>
 #include <ranges>
-#include <fstream>
-#include <mutex>
-#include <filesystem>
-#include "SharedMutex.h"
+
+#include "LogFileManager.h"
 
 #ifdef LOGGER_EXPORTS
 #define LOGGER_DECLSPEC __declspec(dllexport)
@@ -24,10 +22,8 @@ public:
 	void writelog(const std::string& funcName, const std::string& msg);
 private:
     std::string split(std::string_view str, std::string_view delim);
-	std::shared_ptr<SharedMutex> fileMutex;
-	std::vector<std::filesystem::path> existFileList;
-	void findLogFiles(const std::filesystem::path & directory);
-	const std::filesystem::path log_file = "c:\\log\\lastest.log";
+	
+	LogFileManager file_mng;
 };
 
 extern "C" {
