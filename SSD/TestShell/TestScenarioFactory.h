7e92d327f2ef0bed4d6d32cd9686b65811e0a0e4
kimtaekun <cjsrnr7532@naver.com>
2024-06-10 09:39:23 +0900

    Update TestScenarioFactory.h

--- SSD/TestShell/TestScenarioFactory.h
+++ SSD/TestShell/TestScenarioFactory.h
@@ -1,12 +1,6 @@
 #pragma once
 #include <string>
 
-#ifdef DEBUG
-#pragma comment (lib, "../x64/Debug/logger.lib")
-#else
-#pragma comment (lib, "../x64/Release/logger.lib")
-#endif // DEBUG
-
 class TestScenario {
 public:
 	void run(std::string scenario);
